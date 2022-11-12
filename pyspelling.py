ccb0628a1326826e9c9d66c3868a98ba3811a961
Andrew Johnson <andrew.johnson@un.org>
2022-11-12 21:41:18 +0100

    Much improvement.

--- pyspelling.py
+++ pyspelling.py
@@ -6,72 +6,132 @@ from colorama import Fore, Style, Back
 import time
 from collections import namedtuple
 import json
+import typing as typ
+
+
+class Score:
+	def __init__(self, word, result):
+		self.word = word
+		self.result = result
+
+class ScoreBoard:
+	scores = []
+
+	def appendScore(self, score):
+		self.scores.append(score)
+		print(self.scores[0].word)
+
+	def getScores():
+		return self.scores
+
+	def getLastScore():
+		return self.scores[len(self.scores)]
+
 
 def main():
-	os.system('clear')
-	wordFilePath = "resources/words/"
-	soundsFilePath = "resources/sounds/"
-	files = (os.listdir(wordFilePath))
-	random.shuffle(files)
-	score = 0
-	question = 0
-	questions = 10
-	result = 0
-	myfiles = files[0:questions]
-	userName = input("Enter your Name: ")
-	print("Hello: " + userName)
-	for x in range(len(myfiles)):
-		start = time.time()
-		myWord = myfiles[question].split(".")[0]
-		wordSound = multiprocessing.Process(target=playsound, args=(wordFilePath+files[question],))
-		wordSound.start()
-		string = input("Question " + str(question+1) + ": Please spell the word: ")
-		if(len(string)>0):
-			if string == myWord:
-				result = 1
-				print(Fore.GREEN + "Well done!" + Style.RESET_ALL)
-				playsound(soundsFilePath+"correct.m4a")
-			else:
-				result = 0
-				print(Fore.RED + "Wrong answer" + Style.RESET_ALL)
-				playsound(soundsFilePath+"wrong.m4a")
-				print("This word is spelt: " + '\033[1m' + myWord + '\033[0m')
-				print("Please repeat the word 3 times")
-				for y in range(0,3):
-					while string!=myWord:
-						string = input("Attempt " + str(y + 1) + ": ")
-					string = None
-			wordSound.terminate()
-			stop = time.time()
-			score+=calculateScore(myWord,stop-start,result)
-			question+=1
-	print(Back.CYAN + Fore.RED + "Your score is " + str(score) + Style.RESET_ALL)
-	saveScore(userName,round((10*score)/questions))
-	displayLeaderBoard()
+	Entry = namedtuple('PastResult','word points')
+	entry = Entry("Fish",1)
 
-def saveScore(userName,score):
-	f = open("score.txt","a")
-	f.write(userName + ':' + str(score) + '\n')
-	f.close()
+	myScoreboard1 = ScoreBoard()
+	myScoreboard1.appendScore(entry)
 
-def displayLeaderBoard():
-	f = open("score.txt","r")
-	records = f.read().split('\n')
-	leaderBoard = []
-	Entry = namedtuple('Player','name score')
-	print("\n--- Leader Board ---")
-	for x in records:
-		pair = x.split(':')
-		if (len(pair[0])>0):
-			entry = Entry(pair[0],int(pair[1]))
-			leaderBoard.append(entry)
-	leaderBoard.sort(key=lambda x: getattr(x, 'score'),reverse = True)
-	topFivePlayers = leaderBoard[:5]
-	for idx, x in enumerate(topFivePlayers):
-		print(str(idx + 1) + "\t" + x.name + "\t" + str(x.score))
+	print(myScoreboard1.getLastScore.word)
 
-def calculateScore(word,time,result):
-	return round(100*((len(word)/time)*result))
+#	myScoreBoard = ScoreBoard()
+#
+#	myScoreBoard.appendScore(Score("That", 1))
+#	myScoreBoard.appendScore(Score("This", 0))
+#
+#	print (myScoreBoard.getScores)
+#
+#	print(myScoreBoard.getLastScore.word)
+
+
+
+
+
+#def main():
+#	os.system('clear')
+#	wordFilePath = "resources/words/"
+#	soundsFilePath = "resources/sounds/"
+#	files = (os.listdir(wordFilePath))
+#	random.shuffle(files)
+#	score = 0
+#	question = 0
+#	questions = 10
+#	result = 0
+#	myfiles = files[0:questions]
+#	userName = input("Enter your Name: ")
+#	print("Hello: " + userName)
+#	for x in range(len(myfiles)):
+#		start = time.time()
+#		myWord = myfiles[question].split(".")[0]
+#		wordSound = multiprocessing.Process(target=playsound, args=(wordFilePath+files[question],))
+#		wordSound.start()
+#		string = input("Question " + str(question+1) + ": Please spell the word: ")
+#		if(len(string)>0):
+#			if string == myWord:
+#				result = 1
+#				print(Fore.GREEN + "Well done!" + Style.RESET_ALL)
+#				playsound(soundsFilePath+"correct.m4a")
+#			else:
+#				result = 0
+#				print(Fore.RED + "Wrong answer" + Style.RESET_ALL)
+#				playsound(soundsFilePath+"wrong.m4a")
+#				print("This word is spelt: " + '\033[1m' + myWord + '\033[0m')
+#				print("Please repeat the word 3 times")
+#				for y in range(0,3):
+#					while string!=myWord:
+#						string = input("Attempt " + str(y + 1) + ": ")
+#					string = None
+#			wordSound.terminate()
+#			stop = time.time()
+#			score+=calculateScore(myWord,stop-start,result)
+#			saveResult(userName,myWord,result)
+#			question+=1
+#	print(Back.CYAN + Fore.RED + "Your score is " + str(score) + Style.RESET_ALL)
+#	saveScore(userName,round((10*score)/questions))
+#	displayLeaderBoard()
+#
+#def saveResult(iName, iWord, iResult):
+#	resultsPath = "save/"
+#	resultsFile = iName + ".txt"
+#	iFile = open(resultsPath + resultsFile,"r")
+#	records = iFile.read().split('\n')
+#	workingRecords = []
+#	Entry = namedtuple('PastResult','word points')
+#	for x in records:
+#		pair = x.split(':')
+#		if(len(pair[0])>0):
+#			entry = Entry(pair[0],int(pair[1]))
+#			workingRecords.append(entry)
+#	oFile = open(resultsPath + resultsFile,"a")
+#	oFile.write(iWord + ":" + str(iResult) + '\n')
+#	oFile.close()
+#
+#def saveScore(userName,score):
+#	f = open("score.txt","a")
+#	f.write(userName + ':' + str(score) + '\n')
+#	f.close()
+#
+#def displayLeaderBoard():
+#	f = open("score.txt","r")
+#	records = f.read().split('\n')
+#	leaderBoard = []
+#	Entry = namedtuple('Player','name score')
+#	print("\n--- Leader Board ---")
+#	for x in records:
+#		pair = x.split(':')
+#		if (len(pair[0])>0):
+#			entry = Entry(pair[0],int(pair[1]))
+#			leaderBoard.append(entry)
+#	leaderBoard.sort(key=lambda x: getattr(x, 'score'),reverse = True)
+#	topFivePlayers = leaderBoard[:5]
+#	for idx, x in enumerate(topFivePlayers):
+#		print(str(idx + 1) + "\t" + x.name + "\t" + str(x.score))
+#
+#def calculateScore(word,time,result):
+#	return round(100*((len(word)/time)*result))
 
 if __name__ == '__main__':
     main()
---
+++ pyspellingoo.py
@@ -0,0 +1,129 @@
+
+from dataclasses import field, dataclass
+from os import listdir
+from os import getcwd
+import uuid
+
+
+@dataclass
+class Environment:
+    word_path: str = 'resources/words/'
+    sound_path: str = "resources/sounds/"
+    bar: list[str] = field(default_factory=list)
+    file_list: list[str] = field(default_factory=list)
+    email_addresses: list[str] = field(default_factory=list)
+    id: str = field(init=False, default_factory=uuid.uuid4)
+    _search_string: str = field(init=False, repr=False)
+
+    def __post_init__(self) -> None:
+        self._search_string = f"{self.word_path} {self.sound_path}"
+        self.bar.append(".m4a")
+        self.bar.append(".mp3")
+        for file in listdir(self.word_path):
+            self.file_list.append(file)
+
+
+class Results:
+    def __init__(self):
+        self.results = []
+
+    def add_result(self, result):
+        word_found = False
+        for i in self.results:
+            if i.word == result.word:
+                i.score += result.score
+                word_found = True
+        if not word_found:
+            self.results.append(result)
+
+    def get_results(self):
+        self.results.sort()
+        return self.results
+
+
+class Result:
+    def __init__(self, word, score):
+        self.word = word
+        self.score = score
+
+    def __lt__(self, value):
+        if not isinstance(value, Result):
+            raise ValueError("Can't compare a Result to a non-result")
+        return self.score < value.score
+
+
+class Score:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __lt__(self, value):
+        if not isinstance(value, Score):
+            raise ValueError("Can't compare a Score to a non-score")
+        return self.score < value.score
+
+
+class LeaderBoard:
+    def __init__(self):
+        self.scores = []
+
+    def add_score(self, score):
+        self.scores.append(score)
+
+    def get_scores(self):
+        self.scores.sort(reverse=True)
+        return self.scores
+
+
+class Word:
+    def __init__(self, word):
+        self.word = word
+
+    def __str__(self):
+        return f"{self.word}"
+
+    def get_word_file(self):
+        return self.word + ".m4a"
+
+
+class Dictionary:
+    def __init__(self):
+        self.dictionary = []
+
+    def add_word(self, word):
+        self.dictionary.append(word)
+
+    def list_words(self):
+        return self.dictionary
+
+    def get_first_word(self):
+        return self.dictionary[0].get_word_file()
+
+
+print(getcwd())
+
+
+leader_board = LeaderBoard()
+leader_board.add_score(Score("Andrew", 324))
+leader_board.add_score(Score("Oliver", 999))
+leader_board.add_score(Score("Matthew", 938))
+leader_board.add_score(Score("Becky", 332))
+
+print([score.name for score in leader_board.get_scores()])
+print([score.score for score in leader_board.get_scores()])
+
+
+results = Results()
+results.add_result(Result("when", 1))
+results.add_result(Result("one", 1))
+results.add_result(Result("could", 0))
+results.add_result(Result("should", 0))
+results.add_result(Result("one", 1))
+
+print([result.word for result in results.get_results()])
+print([result.score for result in results.get_results()])
+
+
+d = Dictionary()
+env = Environment()
+print(Environment())
